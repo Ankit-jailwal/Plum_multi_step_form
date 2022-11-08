60e58c664c40ada058b1f27f8892f28da56d49e3
Ankit-jailwal <jailwalankit@gmail.com>
2022-11-08 06:06:47 +0530

    Created 4 steps

--- src/LinearStepper.js
+++ src/LinearStepper.js
@@ -1,8 +1,24 @@
+import { Step, StepLabel, Stepper } from '@material-ui/core'
 import React from 'react'
 
 const LinearStepper = () => {
   return (
-    <div>LinearStepper</div>
+    <div>
+        <Stepper activeStep={0}>
+            <Step>
+                <StepLabel>Step 1</StepLabel>
+            </Step>
+            <Step>
+                <StepLabel>Step 2</StepLabel>
+            </Step>
+            <Step>
+                <StepLabel>Step 3</StepLabel>
+            </Step>
+            <Step>
+                <StepLabel>Step 4</StepLabel>
+            </Step>
+        </Stepper>
+    </div>
   )
 }
 
